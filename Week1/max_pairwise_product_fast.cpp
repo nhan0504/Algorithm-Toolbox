c3eb2fed3fa4d325c7808eed8b90781fc3083297
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-10 12:42:40 +0700

    Maximum product of two numbers in a list (Fast version)

---
+++ Week1/max_pairwise_product_fast.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int MaxPairwiseProductFast(vector<int> numbers) {
+	int index1 = 0;
+	for (int i = 1; i != numbers.size(); i++) {
+		if (numbers[i] > numbers[index1]) {
+			index1 = i;
+		}
+	}
+	int index2;
+	if (index1 == 0) {
+		index2 = 1;
+	}
+	else {
+		index2 = 0;
+	}
+	for (int i = 0; i != numbers.size(); i++) {
+		if (i != index1 && numbers[i] > numbers[index2]) {
+			index2 = i;
+		}
+	}
+	int product = numbers[index1] * numbers[index2];
+	return product;
+}
+
+int main() {
+	int n;
+	cout << "Enter list size: ";
+	cin >> n;
+	vector<int> numbers(n);
+	cout << "Enter all the element for list:" << endl;
+	for (int i = 0; i < n; ++i) {
+		cin >> numbers[i];
+	}
+
+	cout << "maximum product of two numbers = " << MaxPairwiseProductFast(numbers);
+	return 0;
+}
