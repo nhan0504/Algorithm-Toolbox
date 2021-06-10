deebd01e8d9c301d9f82bca17c9f5a354ba831ec
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-10 21:28:22 +0700

    Test and fix with large input

--- Week1/max_pairwise_product_fast.cpp
+++ Week1/max_pairwise_product_fast.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-int MaxPairwiseProductFast(vector<int64_t> numbers) {
+int64_t MaxPairwiseProductFast(vector<int64_t> numbers) {
 	int index1 = 0;
 	for (int i = 1; i != numbers.size(); i++) {
 		if (numbers[i] > numbers[index1]) {
@@ -22,10 +22,12 @@ int MaxPairwiseProductFast(vector<int64_t> numbers) {
 		}
 	}
 	int64_t product = numbers[index1] * numbers[index2];
+	cout << "product = " << product << endl;
 	return product;
 }
 
 int main() {
+	//Test with input data
 	int n;
 	cout << "Enter list size: ";
 	cin >> n;
@@ -34,7 +36,13 @@ int main() {
 	for (int i = 0; i < n; ++i) {
 		cin >> numbers[i];
 	}
+	cout << "maximum product of two numbers = " << MaxPairwiseProductFast(numbers) << endl;
 
-	cout << "maximum product of two numbers = " << MaxPairwiseProductFast(numbers);
+	//Test with large input
+	vector<int64_t> large(2 * pow(10,5));
+	for (int64_t i = 1; i <= 2 * pow(10,5); i++) {
+		large[i - 1] = i;
+	}
+	cout << "maximum product of two numbers = " << MaxPairwiseProductFast(large);
 	return 0;
 }
