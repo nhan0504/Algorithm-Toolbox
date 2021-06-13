d3438cc1c039c4c9073059d6eb75ba107f4c2df4
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-13 15:24:06 +0700

    Update using sort function

--- Week1/max_pairwise_product_compact.cpp
+++ Week1/max_pairwise_product_compact.cpp
@@ -5,19 +5,20 @@ using namespace std;
 
 int64_t MaxPairwiseProduct(vector<int> list) {
     sort(list.begin(), list.end());
-    int64_t product = *(list.end() - 1) * *(list.end() - 2);
+    int64_t product = 0;
+    for (int i = 0; i != list[list.size() - 1]; i++) {
+        product += list[list.size() - 2];
+    }
     return product;
 }
 
 int main() {
     int n;
-    cout << "Enter list size: ";
     cin >> n;
     vector<int> numbers(n);
-    cout << "Enter all the element for list:" << endl;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         cin >> numbers[i];
     }
-    cout << "maximum product of two numbers = " << MaxPairwiseProduct(numbers) << "\n";
+    cout << MaxPairwiseProduct(numbers);
     return 0;
 }
