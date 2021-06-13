91388c7a98c7624e40970fe6bcad6954eb099cdf
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-13 15:37:40 +0700

    Update

--- Week1/merge.cpp
+++ Week1/merge.cpp
@@ -1,72 +1,72 @@
-//#include<iostream>
-//#include<vector>
-//using namespace std;
-//
-//void Merge(vector<int>& list1, vector<int>& list2, vector<int>& MergedList) {
-//    if (list1.size() != list2.size() && list1.size() == 0 || list2.size() == 0) {
-//        if (list1.size() < list2.size()) {
-//            for (int i = list1.size(); i != list2.size(); i++) {
-//                MergedList.push_back(list2[i]);
-//            }
-//        }
-//        else if (list1.size() > list2.size()) {
-//            for (int i = list2.size(); i != list1.size(); i++) {
-//                MergedList.push_back(list1[i]);
-//            }
-//        }
-//        return;
-//    }
-//    else if (list1.size() != list2.size() && list1.size() == 0) {
-//        return;
-//    }
-//    int index1 = 0;
-//    for (int i = 1; i != list1.size(); i++) {
-//        if (list1[i] < list1[index1]) {
-//            index1 = i;
-//        }
-//    }
-//    int index2 = 0;
-//    for (int i = 1; i != list2.size(); i++) {
-//        if (list2[i] < list2[index2]) {
-//            index2 = i;
-//        }
-//    }
-//    if (list1[index1] < list2[index2]) {
-//        MergedList.push_back(list1[index1]);
-//        list1.erase(list1.begin() + index1);
-//        MergedList.push_back(list2[index2]);
-//        list2.erase(list2.begin() + index2);
-//    }
-//    else {
-//        MergedList.push_back(list2[index2]);
-//        list2.erase(list2.begin() + index2);
-//        MergedList.push_back(list1[index1]);
-//        list1.erase(list1.begin() + index1);
-//    }
-//    Merge(list1, list2, MergedList);
-//}
-//
-//int main() {
-//    int n;
-//    cout << "Enter list1 size: ";
-//    cin >> n;
-//    vector<int> list1(n);
-//    cout << "Enter all the element for list1:" << endl;
-//    for (int i = 0; i < n; i++) {
-//        cin >> list1[i];
-//    }
-//    int m;
-//    cout << "Enter list2 size: ";
-//    cin >> m;
-//    vector<int> list2(m);
-//    cout << "Enter all the element for list2:" << endl;
-//    for (int i = 0; i < m; i++) {
-//        cin >> list2[i];
-//    }
-//    vector<int> MergedList;
-//    Merge(list1, list2, MergedList);
-//    for (int i = 0; i != MergedList.size(); i++) {
-//        cout << MergedList[i] << "\t";
-//    }
-//    return 0;
-//}
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void Merge(vector<int>& list1, vector<int>& list2, vector<int>& MergedList) {
+    if (list1.size() != list2.size() && list1.size() == 0 || list2.size() == 0) {
+        if (list1.size() < list2.size()) {
+            for (int i = list1.size(); i != list2.size(); i++) {
+                MergedList.push_back(list2[i]);
+            }
+        }
+        else if (list1.size() > list2.size()) {
+            for (int i = list2.size(); i != list1.size(); i++) {
+                MergedList.push_back(list1[i]);
+            }
+        }
+        return;
+    }
+    else if (list1.size() != list2.size() && list1.size() == 0) {
+        return;
+    }
+    int index1 = 0;
+    for (int i = 1; i != list1.size(); i++) {
+        if (list1[i] < list1[index1]) {
+            index1 = i;
+        }
+    }
+    int index2 = 0;
+    for (int i = 1; i != list2.size(); i++) {
+        if (list2[i] < list2[index2]) {
+            index2 = i;
+        }
+    }
+    if (list1[index1] < list2[index2]) {
+        MergedList.push_back(list1[index1]);
+        list1.erase(list1.begin() + index1);
+        MergedList.push_back(list2[index2]);
+        list2.erase(list2.begin() + index2);
+    }
+    else {
+        MergedList.push_back(list2[index2]);
+        list2.erase(list2.begin() + index2);
+        MergedList.push_back(list1[index1]);
+        list1.erase(list1.begin() + index1);
+    }
+    Merge(list1, list2, MergedList);
+}
+
+int main() {
+    int n;
+    cout << "Enter list1 size: ";
+    cin >> n;
+    vector<int> list1(n);
+    cout << "Enter all the element for list1:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> list1[i];
+    }
+    int m;
+    cout << "Enter list2 size: ";
+    cin >> m;
+    vector<int> list2(m);
+    cout << "Enter all the element for list2:" << endl;
+    for (int i = 0; i < m; i++) {
+        cin >> list2[i];
+    }
+    vector<int> MergedList;
+    Merge(list1, list2, MergedList);
+    for (int i = 0; i != MergedList.size(); i++) {
+        cout << MergedList[i] << "\t";
+    }
+    return 0;
+}
