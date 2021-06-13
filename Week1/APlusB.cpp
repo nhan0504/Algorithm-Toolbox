50b220ef9a89186d085c3f361171ce481e233e85
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-13 10:03:55 +0700

    Update new version to pass all test cases

--- Week1/APlusB.cpp
+++ Week1/APlusB.cpp
@@ -8,8 +8,7 @@ int sum_of_two_digits(int first_digit, int second_digit) {
 int main() {
     int a = 0;
     int b = 0;
-    cout << "Enter two number: ";
     cin >> a >> b;
-    cout << a << " + " << b << " = " << sum_of_two_digits(a, b);
+    cout << sum_of_two_digits(a,b);
     return 0;
 }
