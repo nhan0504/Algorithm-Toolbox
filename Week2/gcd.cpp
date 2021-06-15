c2cb27b50064a671e7387523d781cb66f933a6b6
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-15 20:52:26 +0700

    Slow version to find greatest common diviser

---
+++ Week2/gcd.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int gcd_naive(int a, int b) {
+  int current_gcd = 1;
+  for (int d = 2; d <= a && d <= b; d++) {
+    if (a % d == 0 && b % d == 0) {
+      if (d > current_gcd) {
+        current_gcd = d;
+      }
+    }
+  }
+  return current_gcd;
+}
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << gcd_naive(a, b) << std::endl;
+  return 0;
+}
