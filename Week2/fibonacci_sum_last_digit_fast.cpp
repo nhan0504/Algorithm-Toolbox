c80d5972eff435c614c22d9acb83ee959d13a79c
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-17 09:33:30 +0700

    Faster version - still fail some test cases with large input

---
+++ Week2/fibonacci_sum_last_digit_fast.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci_last_digit_fast(int n) {
+	if (n <= 1) {
+		return n;
+	}
+	int64_t f0 = 0;
+	int64_t f1 = 1;
+	int64_t f2 = 0;
+	int64_t sum = 1;
+	int n2 = (n + 1 - 2) % 3;
+	int n1 = n + 1 - 2 - n2;
+	for (int i = 0; i < n1; i += 3) {
+		f2 = f1 + f0;
+		sum = sum + f2;
+		f0 = f1 + f2;
+		sum = sum + f0;
+		f1 = f2 + f0 ;
+		sum = sum + f1;
+		
+		if (sum > 922337205) {
+			sum = sum % 10;
+			f0 %= 10; 
+			f1 %= 10;
+			f2 %= 10;
+		}
+	}
+	for (int i = 0; i < n2; i++) {
+		f2 = (f0 + f1) % 10;
+		sum = sum + f2;
+		f0 = f1; f1 = f2;
+	}
+	return sum%10;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << fibonacci_last_digit_fast(n) << '\n';
+}
