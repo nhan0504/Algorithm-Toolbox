e6d692c8be48bfc323ca097b0ac9cea66407451c
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-19 00:17:46 +0700

    Use Pisano period

--- Week2/fibonacci_sum_squares_fast.cpp
+++ Week2/fibonacci_sum_squares_fast.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 
+vector<int> get_remainder(int64_t n) {
+	vector<int> remainder;
+	remainder.push_back(0);
+	remainder.push_back(1);
+	int a = 0;
+	int b = 1;
+	for (int i = 0; i < 58; i++) {
+		int c = a;
+		a = b;
+		b = (c + b) % n;
+		remainder.push_back(b);
+	}
+	return remainder;
+}
+
 uint64_t fibonacci_sum_squares(int64_t n) {
+	vector<int> remainder = get_remainder(10);
+	int size = remainder.size();
     if (n <= 1) {
         return n;
     }
-    int64_t previous = 0;
-    int64_t current = 1;
-    for (int i = 0; i < n; i++) {
-        int64_t temp_previous = previous;
-        previous = current;
-        current = temp_previous + current;
-        if (current > 922337205) {
-            current %= 10;
-            previous %= 10;
-        }
-    }
-    int64_t sum = (int64_t)current * previous;
+	int64_t end = n % size;
+    int64_t sum = (int64_t)remainder[end] * remainder[end - 1];
     return sum % 10;
 }
 
