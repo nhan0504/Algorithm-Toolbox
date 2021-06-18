38d9e6a686f681518b095aa6a8d2caa510bd444a
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-19 00:02:37 +0700

    Use Pisano period - still fail one test case

--- Week2/fibonacci_partial_sum.cpp
+++ Week2/fibonacci_partial_sum.cpp
@@ -16,75 +16,16 @@ vector<int> get_remainder(int64_t n) {
 	return remainder;
 }
 
-uint64_t get_fibonacci_partial_sum(int64_t from, int64_t to) {
+int get_fibonacci_partial_sum(int64_t from, int64_t to) {
 	vector<int> remainder = get_remainder(10);
-	for (int i = 0; i < 60; i++) {
-		cout << remainder[i] << " ";
-	}
-	cout << endl;
-    uint64_t f0 = 0;
-    uint64_t f1 = 1;
-    uint64_t f2 = 0;
-	uint64_t sum = 0;
-    uint64_t n = to - from + 1;
-	if (from == 0 && to == 0) {
-		return 0;
-	}
-	if (from == 0) {
-		n -= 2;
-		sum = 1;
-		from = 2;
-	}
-	if (from == 1) {
-		n -= 1;
-		sum = 1;
-		from = 2;
-	}
-	
-	//Calculate last digit before Fm
-	uint64_t s2 = (from - 2) % 3;	
-	uint64_t s1 = from - 2 - s2;
-	//cout << "s1 = " << s1 << endl;
-	//cout << "s2 = " << s2 << endl;
-	for (uint64_t i = 0; i < s1; i += 3) {
-		f2 = f1 + f0;
-		f0 = f1 + f2;
-		f1 = f2 + f0;
-		if (f2 > 922337205) {
-			//f2 %= 10;
-			f1 %= 10;
-			f0 %= 10;
-		}
-	}
-	for (int i = 0; i < s2; i++) {
-		f2 = (f0 + f1) % 10;
-		f0 = f1; f1 = f2;
-	}
-	//cout << "f0 = " << f0 % 10 << endl;
-	//cout << "f1 = " << f1 % 10 << endl;
-
-	//Calculate last digit of sum from Fm to Fn
-	uint64_t n2 = n % 3;
-	uint64_t n1 = n - n2;
-	for (uint64_t i = 0; i < n1; i += 3) {
-		f2 = f1 + f0;
-		sum = sum + f2;
-		f0 = f1 + f2;
-		sum = sum + f0;
-		f1 = f2 + f0;
-		sum = sum + f1;
-
-		if (sum > 922337205) {
-			sum = sum % 10;
-			//f2 %= 10;
-			f1 %= 10;
-			f0 %= 10;
-		}
-	}
-	for (int i = 0; i < n2; i++) {
-		f2 = (f0 + f1) % 10;
-		sum = sum + f2;
-		f0 = f1; f1 = f2;
+	int size = remainder.size();
+	int64_t start = from % size;
+	cout << "start = " << start << endl;
+	int64_t end = to % size;
+	cout << "end = " << end << endl;
+	int64_t sum = 0;
+	for (int i = start; i <= end; i++) {
+		sum += remainder[i];
 	}
     return sum % 10;
 }
