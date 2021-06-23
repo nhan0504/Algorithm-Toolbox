04858d295735b7ec46ca69485b1a5a07b7ed9dc1
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-23 23:25:24 +0700

    Fix sort algorithm - Debug version

--- Week4/sorting.cpp
+++ Week4/sorting.cpp
@@ -7,12 +7,12 @@ using namespace std;
 void randomized_quick_sort(vector<int>& a, int s, int e) {
     if (s >= e) {
         return;
-    } 
+    }
     int ranIndex = s + rand() % (e - s + 1);
     int x = a[ranIndex];
     cout << "x = " << a[ranIndex] << endl;
     int m1 = s;
-    while(a[m1] != x){
+    while (a[m1] != x) {
         m1++;
     }
     cout << "m1 = " << m1 << endl;
@@ -36,22 +36,45 @@ void randomized_quick_sort(vector<int>& a, int s, int e) {
             }
             else if (i == m2 + 1) {
                 swap(a[i], a[m1]);
+                for (int j = 0; j < a.size(); j++) {
+                    cout << a[j] << " ";
+                }
+                cout << endl;
                 m1++;
-                m2 = m1;
+                cout << "m1 = " << m1 << endl;
+                m2++;
+                cout << "m2 = " << m2 << endl;
             }
-            
+
         }
         else if (m1 != i && a[i] == x) {
             m2++;
-            swap(a[i], a[m2]);
+            if (i != m1 + 1) {
+                swap(a[i], a[m2]);
+            }
             for (int j = 0; j < a.size(); j++) {
                 cout << a[j] << " ";
             }
             cout << endl;
         }
+        else if (a[i] > x && i < m1) {
+            swap(a[i], a[m2]);
+            for (int j = 0; j < a.size(); j++) {
+                cout << a[j] << " ";
+            }
+            cout << endl;
+            m1--;
+            cout << "m1 = " << m1 << endl;
+            m2--;
+            cout << "m2 = " << m2 << endl;
+        }
     }
-    randomized_quick_sort(a, s, m1 - 1);
-    randomized_quick_sort(a, m2 + 1, e);
+    if (m1 != 0){
+        randomized_quick_sort(a, s, m1 - 1);
+    }
+    if (m2 != e) {
+        randomized_quick_sort(a, m2 + 1, e);
+    }
 }
 
 int main() {
