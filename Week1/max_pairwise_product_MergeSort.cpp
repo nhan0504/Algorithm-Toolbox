522c9dd7836ae426da092802b1da2b08d5387feb
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-24 17:06:20 +0700

    Improve readability

--- Week1/max_pairwise_product_MergeSort.cpp
+++ Week1/max_pairwise_product_MergeSort.cpp
@@ -2,26 +2,26 @@
 #include<vector>
 using namespace std;
 void Merge(vector<int>& list1, vector<int>& list2, vector<int>& MergedList) {
-    if (list1.size() == 0 || list2.size() == 0) {
-        if (list1.size() < list2.size()) {
-            for (int i = list1.size(); i != list2.size(); i++) {
-                MergedList.push_back(list2[i]);
-            }
-        }
-        else if (list1.size() > list2.size()) {
-            for (int i = list2.size(); i != list1.size(); i++) {
-                MergedList.push_back(list1[i]);
-            }
-        }
-        return;
-    }
-    if (list1[0] < list2[0]) {
+    if (list1[0] <= list2[0]) {
         MergedList.push_back(list1[0]);
         list1.erase(list1.begin());
     }
     else {
         MergedList.push_back(list2[0]);
-        list2.erase(list2.begin());  
+        list2.erase(list2.begin());
+    }
+
+    if (list1.size() == 0) {
+        for (int i = list1.size(); i != list2.size(); i++) {
+            MergedList.push_back(list2[i]);
+        }
+        return;
+    }
+    if (list2.size() == 0) {
+        for (int i = list2.size(); i != list1.size(); i++) {
+            MergedList.push_back(list1[i]);
+        }
+        return;
     }
     Merge(list1, list2, MergedList);
 }
