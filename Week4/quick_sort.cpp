aae1f5764886da1e94c40eb7cfb1b13de65ebcab
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-26 14:33:39 +0700

    Pass all test cases

--- Week4/quick_sort.cpp
+++ Week4/quick_sort.cpp
@@ -23,70 +23,35 @@ void randomized_quick_sort(vector<int>& a, int s, int e) {
     }
     int m1 = s + rand() % vector_size;
     int x = a[m1];
-    cout << "x = " << x << endl;
     int m2 = m1;
     int p = s;
     while (p < m1) {
-        cout << "a[" << p << "] = " << a[p] << endl;
         if (a[p] == x) {
             swap(a[p], a[m1 - 1]);
             m1--;
-            cout << "m1 = " << m1 << endl;
-            cout << "m2 = " << m2 << endl;
-            for (int i = 0; i <= e; i++) {
-                cout << a[i] << ' ';
-            }
-            cout << endl;
         }
         else if (a[p] > x) {
             swap_3_numbers(a, m2, m1 - 1, p);
             m1--;
             m2--;
-            cout << "m1 = " << m1 << endl;
-            cout << "m2 = " << m2 << endl;
-            for (int i = 0; i <= e; i++) {
-                cout << a[i] << ' ';
-            }
-            cout << endl;
         }
         else if (a[p] < x) {
             p++;
-            for (int i = 0; i <= e; i++) {
-                cout << a[i] << ' ';
-            }
-            cout << endl;
         }
     }
     int q = e;
     while (q > m2) {
-        cout << "a[" << q << "] = " << a[q] << endl;
         if (a[q] == x) {
             swap(a[q], a[m2 + 1]);
             m2++;
-            cout << "m1 = " << m1 << endl;
-            cout << "m2 = " << m2 << endl;
-            for (int i = 0; i <= e; i++) {
-                cout << a[i] << ' ';
-            }
-            cout << endl;
         }
         else if (a[q] < x) {
             swap_3_numbers(a, m1, m2 + 1, q);
             m1++;
             m2++;
-            cout << "m1 = " << m1 << endl;
-            cout << "m2 = " << m2 << endl;
-            for (int i = 0; i <= e; i++) {
-                cout << a[i] << ' ';
-            }
-            cout << endl;
         }
         else if (a[q] > x) {
             q--;
-            for (int i = 0; i <= e; i++) {
-                cout << a[i] << ' ';
-            }
-            cout << endl;
         }
     }
     randomized_quick_sort(a, s, m1 - 1);
