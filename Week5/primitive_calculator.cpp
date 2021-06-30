480b9c60565f05e969bcd07e91db4b1d67054181
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-30 10:47:28 +0700

    Reconstruct the sequence of numbers to get to n

--- Week5/primitive_calculator.cpp
+++ Week5/primitive_calculator.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int optimal_sequence(int n) {
+vector<int> optimal_sequence(int n) {
     vector<int> sequence;
     vector<int> NumSteps(n + 1, 0);
     for (int i = 2; i <= n; i++) {
@@ -23,16 +23,31 @@ int optimal_sequence(int n) {
         }
         NumSteps[i] = min;
     }
-    return NumSteps[n];
+    sequence.push_back(n);
+    while (n != 1) {
+        if (NumSteps[n - 1] + 1 == NumSteps[n]) {
+            sequence.push_back(n - 1);
+            n = n - 1;
+        }
+        else if (n % 2 == 0 && NumSteps[n / 2] + 1 == NumSteps[n]) {
+            sequence.push_back(n / 2);
+            n = n / 2;
+        }
+        else if (n % 3 == 0 && NumSteps[n / 3] + 1 == NumSteps[n]) {
+            sequence.push_back(n / 3);
+            n = n / 3;
+        }
+    }
+    reverse(sequence.begin(), sequence.end());
+    return sequence;
 }
 
 int main() {
     int n;
     cin >> n;
-    //vector<int> sequence = optimal_sequence(n);
-    //cout << sequence.size() - 1 << std::endl;
-    /*for (size_t i = 0; i < sequence.size(); ++i) {
+    vector<int> sequence = optimal_sequence(n);
+    cout << sequence.size() - 1 << endl;
+    for (size_t i = 0; i < sequence.size(); i++) {
         cout << sequence[i] << " ";
-    }*/
-    cout << optimal_sequence(n);
+    }
 }
