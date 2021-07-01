fa964af621e48239b500d2771797b821dfd5891c
Nhan0504 <ttn0504ltt@gmail.com>
2021-07-01 10:52:50 +0700

    Pass all test cases

--- Week6/knapsack.cpp
+++ Week6/knapsack.cpp
@@ -7,18 +7,13 @@ int optimal_weight(int W, const vector<int>& w) {
     vector<int> initialize(w.size() + 1, 0);
     vector<vector<int>> value(W + 1, initialize);
     for (int j = 1; j <= w.size(); j++) {
-        cout << "j = " << j << endl;
         for (int i = 1; i <= W; i++) {
-            cout << "i = " << i << endl;
-            //exclude jth item
             value[i][j] = value[i][j - 1];
             if (w[j - 1] <= i) {
-                //include jth item
                 int val = value[i - w[j - 1]][j - 1] + w[j - 1];
                 value[i][j] = max(value[i][j], val);
             }
-            cout << "value[" << i << "][" << j << "] = " << value[i][j] << endl;
-        }     
+        }
     }
     return value[W][w.size()];
 }
@@ -28,7 +23,7 @@ int main() {
     cin >> W >> n;
     vector<int> w(n);
     for (int i = 0; i < n; i++) {
-    cin >> w[i];
+        cin >> w[i];
     }
     cout << optimal_weight(W, w) << '\n';
 }
